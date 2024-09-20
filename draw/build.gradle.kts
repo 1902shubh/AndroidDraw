56535895ad87943204e94d49204c42affe74e2fd
Shubham Pandey <pro.1902shubh@gmail.com>
2024-09-21 00:56:40 +0530

    java version

--- draw/build.gradle.kts
+++ draw/build.gradle.kts
@@ -27,17 +27,17 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
 }
 
 
 group = "com.papayacoders.draw"
-version = "1.0.4"
+version = "1.0.5"
 
 
 //tasks.named("publishToMavenLocal") {
